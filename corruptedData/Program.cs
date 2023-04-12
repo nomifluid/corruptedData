8cb631a766a3631851f9f4dbcb4a1d77e4ef890f
theo <114231187+nomifluid@users.noreply.github.com>
2023-04-12 10:55:57 +0200

    first commit

---
+++ corruptedData/Program.cs
@@ -0,0 +1,22 @@
+﻿StreamReader sr = new StreamReader("../../../corrupted_data.txt");
+
+string GetField(string? l, int n)
+{
+    string s = l.Split(';')[n].ToString();
+    if (s == "?") return "unknown";
+    return s;
+}
+bool OutputLine()
+{
+    string l = sr.ReadLine();
+    if (l == null) return false;
+    Console.WriteLine($"{GetField(l, 2)} ({GetField(l, 0)}):\n\t {GetField(l, 8)} ({GetField(l, 4)})\n\t Vklad: {GetField(l, 7)} EUR");
+    return true;
+}
+
+while (OutputLine())
+{
+
+}
+
+Console.ReadKey();
